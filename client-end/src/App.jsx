739ac2700e6803df7b506393a5fded409d781032
nkorada21 <narendrakorada.21@gmail.com>
2025-11-22 02:59:34 -0600

    Update the App.jsx file and Navbar.jsx of landing page with clerk authentication

--- client-end/src/App.jsx
+++ client-end/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import BlogTitles from './pages/BlogTitles'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import WriteArticle from './pages/WriteArticle'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ResumeAnalyzer from './pages/ResumeAnalyzer'
+import Community from './pages/Community'
 
 const App = () => {
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard/>} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='resume-analyzer' element={<ResumeAnalyzer />} />
+          <Route path='community' element={<Community />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
---
+++ client-end/src/pages/ResumeAnalyzer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResumeAnalyzer = () => {
+  return (
+    <div>
+      <h1>ResumeAnalyzer</h1>
+    </div>
+  )
+}
+
+export default ResumeAnalyzer
