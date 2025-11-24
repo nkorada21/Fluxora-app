fafccb135ebcccbf325526f2da6ae49bd0e4f7f2
nkorada21 <narendrakorada.21@gmail.com>
2025-11-24 02:55:25 -0600

    Update the Layout file

--- client-end/src/pages/Layout.jsx
+++ client-end/src/pages/Layout.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { Menu, X } from 'lucide-react'
 
 const Layout = () => {
+
+  const navigate = useNavigate()
+  const [sidebar, setSidebar] = useState(false)
+
   return (
-    <div>
-      <h1>Layout</h1>
+    <div className='flex flex-col items-start justify-start h-screen'>
+      
+      <nav>
+        <img src={assets.logo} alt="NavLogo" onClick={()=>navigate('/')}/>
+        {
+            sidebar ? <X className='w-6 h-6 text-gray-600 sm:hidden' onClick={() => setSidebar(false)}/>
+            : <Menu className='w-6 h-6 text-gray-600 sm:hidden' onClick={() => setSidebar(true)}/>
+        }
+      </nav>
+
       <Outlet />
     </div>
   )
