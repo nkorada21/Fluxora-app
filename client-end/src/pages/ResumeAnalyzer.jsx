462d6dfb5408a94efa7189ef4b5e0e2d92f2c4d4
nkorada21 <narendrakorada.21@gmail.com>
2025-11-28 02:37:31 -0600

    Update backend server folder

--- client-end/src/pages/ResumeAnalyzer.jsx
+++ client-end/src/pages/ResumeAnalyzer.jsx
@@ -1,9 +1,54 @@
+import { FileText, Sparkles } from 'lucide-react'
 import React from 'react'
+import { useState } from 'react'
 
 const ResumeAnalyzer = () => {
+
+  const [input, setInput ] = useState('')
+        
+          const onSubmitHandler = async (e)=> {
+            e.preventDefault();
+          }
+
   return (
-    <div>
-      <h1>ResumeAnalyzer</h1>
+    <div className='h-full overflow-y-scroll p-6 flex items-start 
+    flex-wrap gap-4 text-slate-700'>
+      {/* Left Section */}
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border
+      border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#00DA83]'/>
+          <h1 className='text-xl font-semibold'>AI-Powered Resume Review</h1>
+        </div>
+        <p className='mt-6 text-sm font-medium'> Upload Your Resume for Analysis </p>
+        <input onChange={(e)=>setInput(e.target.files[0])} type="file" accept='.pdf, .doc, .docx' className='w-full p-2 px-3 mt-2 outline-none text-sm 
+        rounded-md border border-gray-300 text-gray-600' required/>
+
+        <p className='text-xs text-gray-500 font-light mt-1'> Upload your resume in PDF, DOC, or DOCX format only. </p>
+        
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] 
+        text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <FileText className='w-5'/>
+          Analyze My Resume
+        </button>
+        
+      </form>
+      {/* Right Section */}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border
+      border-gray-200 min-h-96 max-h-[700px]'>
+
+          <div className='flex items-center gap-3'>
+            <FileText className='w-5 h-5 text-[#00DA83]'/>
+            <h1 className='text-xl font-semibold'>Your Resume Analysis</h1>
+          </div>
+
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+              <FileText className='w-9 h-9'/>
+              <p> Start by uploading your resume, then let AI analyze it for you. </p>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
