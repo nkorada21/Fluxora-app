a43bb54b2e217aeb5d4558fec2fc8af53662268d
nkorada21 <narendrakorada.21@gmail.com>
2025-12-02 03:11:32 -0600

    update the controllers with blog title generator

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -2,6 +2,7 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -36,6 +37,91 @@ export const generateArticle = async (req, res)=>{
 
     await sql` INSERT INTO CREATIONS (user_id, prompt, content, type)
     VALUES (${userId}, ${prompt}, ${content}, 'article')`;
+
+    if(plan !== 'premium'){
+        await clerkClient.users.updateUserMetadata(userId, {
+            privateMetadata:{
+                free_usage: free_usage + 1
+            }
+        })
+    }
+
+    res.json({ success: true, content })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const generateBlogTitle = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { prompt } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if(plan !== 'premium' && free_usage >= 10) {
+            return res.json({success: false, message: "You’ve reached your limit. Upgrade to keep creating."})
+        }
+        
+        const response = await AI.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: 100,
+        });
+        
+        
+        const content = response.choices[0].message.content
+
+    await sql` INSERT INTO CREATIONS (user_id, prompt, content, type)
+    VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+    if(plan !== 'premium'){
+        await clerkClient.users.updateUserMetadata(userId, {
+            privateMetadata:{
+                free_usage: free_usage + 1
+            }
+        })
+    }
+
+    res.json({ success: true, content })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const generateImage = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { prompt, publish } = req.body;
+        const plan = req.plan;
+
+        if(plan !== 'premium') {
+            return res.json({success: false, message: "Upgrade to premium to unlock this feature."})
+        }
+
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+        const {data} = await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
+            headers: {'x-api-key': process.env.CLIPDROP_API_KEY,},
+            responseType: "arraybuffer",
+        })
+
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+        
+
+    await sql` INSERT INTO CREATIONS (user_id, prompt, content, type)
+    VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
 
     if(plan !== 'premium'){
         await clerkClient.users.updateUserMetadata(userId, {
