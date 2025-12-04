e67febcdfc9da2125dce202193a9bedd7f06778f
nkorada21 <narendrakorada.21@gmail.com>
2025-12-03 19:27:01 -0600

    Update the App.jsx

--- client-end/src/App.jsx
+++ client-end/src/App.jsx
@@ -9,8 +9,6 @@ import RemoveBackground from './pages/RemoveBackground'
 import RemoveObject from './pages/RemoveObject'
 import ResumeAnalyzer from './pages/ResumeAnalyzer'
 import Community from './pages/Community'
-import { useAuth } from '@clerk/clerk-react'
-import { useEffect } from 'react'
 import {Toaster} from 'react-hot-toast'
 
 const App = () => {
